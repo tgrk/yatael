3df0a316acc271b0fbafed91c61faed95c7c5637
Martin Wiso <martin@wiso.cz>
2016-04-18 20:36:45 +0200

    Add authorization helper that wraps auth API

---
+++ src/yatael_auth.erl
@@ -0,0 +1,49 @@
+%%%----------------------------------------------------------------------------
+%%% @author Martin Wiso <tajgur@gmail.com>
+%%% @doc
+%%% Authentification helper for Twitter REST API v1.1 authorization
+%%% @end
+%%%----------------------------------------------------------------------------
+-module(yatael_auth).
+
+%% API
+-export([ authorize/1
+        , unauthorize/0
+        ]).
+
+%%%============================================================================
+%%% API
+%%%============================================================================
+-spec authorize(map()) -> {ok, map()} | {error, term()}.
+authorize(Map) ->
+    AccessToken = maps:get(<<"oauth_token">>, Map, undefined),
+    Verifier    = maps:get(<<"oauth_verifier">>, Map, undefined),
+    CallbackURI = maps:get(<<"callback_uri">>, Map, undefined),
+    case {AccessToken, Verifier, CallbackURI} of
+        {_, _, undefined} ->
+            {error, missing_callback_uri};
+        {undefined, _, _} ->
+            {error, missing_token};
+        {_, undefined, _} ->
+            {error, missing_verifier};
+        {undefined, undefined, _} ->
+            ok = yatael:request_token(CallbackURI),
+            {ok, Creds} = yatael:get_oauth_credentials(),
+            {ok, #{<<"oauth_token">> =>
+                       maps:get(<<"access_token">>, Creds)}};
+        {_, _, _} ->
+            case yatael:get_access_token(AccessToken, Verifier) of
+                ok ->
+                    yatael:verify_credentials([{skip_status, true}]);
+                Error ->
+                    Error
+            end
+    end.
+
+-spec unauthorize() -> ok | no_return().
+unauthorize() ->
+    yatael:unauthorize().
+
+%%%============================================================================
+%%% Internal functions
+%%%============================================================================
